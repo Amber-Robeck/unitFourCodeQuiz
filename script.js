e60bebf65860ff1624dfbc0bb5f3573fdc56a5a6
Amber Robeck <arr5533@gmail.com>
2021-12-17 16:33:58 -0600

    added a countdown timer in the start click function and set to clear at zero

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 var startButton = document.getElementById("start");
+var timerE1 = document.getElementById("displayTimer");
+var timer = 10;
 
-startButton.addEventListener("click", function(event) {
-    event.preventDefault();
+
+
+//start button starts timer and it stops when hits 0
+
+startButton.addEventListener("click", function() {
     console.log(startButton)
-})
+
+    var interval = setInterval(function () {
+        timer--;
+        console.log(timer)
+        if (timer === 0) {
+                clearInterval(interval);
+            } 
+            
+         } ,1000);
+        });
